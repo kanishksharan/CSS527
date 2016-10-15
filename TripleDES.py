42be72d34a8eba26ce8ba2e62fb744220eede5bf
kanishksharan <kanishk.sharan.ds@gmail.com>
2016-10-15 16:58:02 -0700

    Update TripleDES.py

--- TripleDES.py
+++ TripleDES.py
@@ -76,7 +76,7 @@ class msgEncrypt(userInputs): # This class will perform all the encryption opera
 
 	# For every block of 64-bits plaintext, there must be an unique IV
 
-	totalPlainTextBlocks = int(len(concatPlainText)/64) # Gives the totl number of 64-bits Plain Text blocks required
+	totalPlainTextBlocks = int(len(concatPlainText)/64) # Gives the total number of 64-bits Plain Text blocks required
 	IV_List = list()
 
 	# Now generating 64-bit Initialization Vector (IV)
@@ -156,11 +156,170 @@ class msgEncrypt(userInputs): # This class will perform all the encryption opera
 
 					print (intEboxRight, len(streboxRight))
 					print (intkeyE1,len(keyE1))
-					xorRK = bin ((intEboxRight) ^ (intkeyE1))
+					xorRK = bin ((intEboxRight) ^ (intkeyE1))[2:].zfill(48)
 
 					# Extract only 48-bits from the XoR output
 					xorRK = xorRK[0:48]
 					print(xorRK,len(xorRK))
+
+					# Now implement S-Box
+					# Divide the 48-bits into 8 parts of 6-bits each
+
+					block1 = xorRK[0:6]
+					block2 = xorRK[6:12]
+					block3 = xorRK[12:18]
+					block4 = xorRK[18:24]
+					block5 = xorRK[24:30]
+					block6 = xorRK[30:36]
+					block7 = xorRK[36:42]
+					block8 = xorRK[42:48]
+
+					print (block1)
+					print (block2)
+					print (block3)
+					print (block4)
+					print (block5)
+					print (block6)
+					print (block7)
+					print (block8)
+
+					superBlock = list([block1,block2,block3,block4,block5,block6,block7,block8])
+					print (superBlock)
+					print (superBlock[1])
+
+					# Creating the 8 S-Boxes
+
+					sBox1 = (14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+					           0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+					           4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+					           15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13)
+
+
+					sBox2 = (15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+					           3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+					           0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+					           13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9)
+
+					sBox3 = (10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+					           13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+					           13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+					           1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12)
+
+					sBox4 = (7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+					           13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+					           10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+					           3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14)
+
+
+					sBox5 = (2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+					           14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+					           4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+					           11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3)
+
+					sBox6 = (12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+					           10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+					           9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+					           4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13)
+
+					sBox7 = (4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+					           13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+					           1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+					           6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12)
+
+					sBox8 = (13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+					           1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+					           7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+					           2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11)
+
+					# Create 8 2-D Arrays to store the table values
+					row = 4
+					col = 16
+
+					table1 = [[0 for c in range (col)] for r in range (row)]
+					table2 = [[0 for c in range (col)] for r in range (row)]
+					table3 = [[0 for c in range (col)] for r in range (row)]
+					table4 = [[0 for c in range (col)] for r in range (row)]
+					table5 = [[0 for c in range (col)] for r in range (row)]
+					table6 = [[0 for c in range (col)] for r in range (row)]
+					table7 = [[0 for c in range (col)] for r in range (row)]
+					table8 = [[0 for c in range (col)] for r in range (row)]
+
+					# Filling elements in table1 from sBox1
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table1[r][c] = sBox1[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table2 from sBox2
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table2[r][c] = sBox2[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table3 from sBox3
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table3[r][c] = sBox3[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table4 from sBox4
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table4[r][c] = sBox4[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table5 from sBox5
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table5[r][c] = sBox5[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table6 from sBox6
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table6[r][c] = sBox6[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table7 from sBox7
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table7[r][c] = sBox7[i]
+					        i += 1
+					        continue
+
+					# Filling elements in table8 from sBox8
+					i = 0
+					for r in range (0,4):
+					    for c in range (0,16):
+					        table8[r][c] = sBox8[i]
+					        i += 1
+					        continue
+
+					left1,right1, middle1 = block1[0],block1[-1],block1[1:-1]
+					left2,right2, middle2 = block1[0],block1[-1],block1[1:-1]
+					left3,right3, middle3 = block1[0],block1[-1],block1[1:-1]
+					left4,right4, middle4 = block1[0],block1[-1],block1[1:-1]
+					left5,right5, middle5 = block1[0],block1[-1],block1[1:-1]
+					left6,right6, middle6 = block1[0],block1[-1],block1[1:-1]
+					left7,right7, middle7 = block1[0],block1[-1],block1[1:-1]
+					left8,right8, middle8 = block1[0],block1[-1],block1[1:-1]
+
+					
+					
+
 	
 
 		
