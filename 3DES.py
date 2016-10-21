e25770a7c427703ac196260827658d5dae890ef3
kanishksharan <kanishk.sharan.ds@gmail.com>
2016-10-21 13:15:55 -0700

    Update 3DES.py

--- 3DES.py
+++ 3DES.py
@@ -109,6 +109,8 @@ roundKey16 = ""
 binPlainText = ""
 lstIV = list()
 feedEBox = list()
+lstLeftBits = list()
+lstxorRK = list()
 
 
 # Creating the method to Generate 16 round keys
@@ -343,8 +345,8 @@ def IP():
         tempStr = "".join(tempShift)
         feedEBox.append(tempStr)
         tempShift.clear()
-    print(feedEBox)
-    print(len(feedEBox))
+    # print(feedEBox)
+    # print(len(feedEBox))
     return feedEBox
 
 
@@ -353,15 +355,16 @@ pkcs5()
 IP()
 
 lstRoundKey = (
-[roundKey1, roundKey2, roundKey3, roundKey4, roundKey5, roundKey6, roundKey7, roundKey8, roundKey9, roundKey10,
- roundKey11, roundKey12, roundKey13, roundKey14, roundKey15, roundKey16])
+    [roundKey1, roundKey2, roundKey3, roundKey4, roundKey5, roundKey6, roundKey7, roundKey8, roundKey9, roundKey10,
+     roundKey11, roundKey12, roundKey13, roundKey14, roundKey15, roundKey16])
 
 
 def EBox():
-
-
+    global lstLeftBits
+    global lstxorRK
     eboxRightBits = list()
-    stringRBits = ""
+    leftBits = ""
+
     Ebox = [32, 1, 2, 3, 4, 5,
             4, 5, 6, 7, 8, 9,
             8, 9, 10, 11, 12, 13,
@@ -386,8 +389,72 @@ def EBox():
             intxorRK = intRBits ^ keyX
             xorRK = bin(intxorRK)[2:].zfill(48)
             eboxRightBits.clear()
-            print(xorRK)
-            print(len(xorRK))
+            # print(xorRK)
+            # print(len(xorRK))
+        lstxorRK.append(xorRK)
+        lstLeftBits.append(leftBits)
+
 
 EBox()
-print(lstRoundKey)
+
+
+# print(lstRoundKey)
+
+def SBox():  # This function performs the tedious S-Box implementation
+
+    lstLeftBits
+    lstxorRK
+
+    sBox1 = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+             0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+             4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+             15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+
+    sBox2 = [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+             3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+             0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+             13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+
+    sBox3 = [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+             13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+             13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+             1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+
+    sBox4 = [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+             13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+             10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+             3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+
+    sBox5 = [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+             14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+             4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+             11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+
+    sBox6 = [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+             10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+             9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+             4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+
+    sBox7 = [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+             13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+             1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+             6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+
+    sBox8 = [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+             1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+             7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+             2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+
+
+# Divide the 48-bits into 8 parts of 6-bits each
+    for blocks in lstxorRK:
+        block1 = blocks[0:6]
+        block2 = blocks[6:12]
+        block3 = blocks[12:18]
+        block4 = blocks[18:24]
+        block5 = blocks[24:30]
+        block6 = blocks[30:36]
+        block7 = blocks[36:42]
+        block8 = blocks[42:48]
+
+SBox()
